abbecdf827fc6d8baea1ea11d360d16a5a7be39f
Renato Bessa <bessache@outlook.com>
2020-04-13 19:03:55 -0300

    commit alua ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+
 const app = express();
 
-app.get('/',(request, response)=>{
-  return response.json({message:'Hello GoStack'});
-})
+app.get('/', (request, response) => response.json({ message: 'Hello GoStack' }));
 
-app.listen(3333, ()=>{
+app.listen(3333, () => {
   console.log('Server start on port 333!');
 });
